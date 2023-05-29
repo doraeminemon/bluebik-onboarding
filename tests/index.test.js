e0b8d12a5332f0b67535c3a68ee8dfecbbb4b8bd
Tyson Do <doraeminemon@gmail.com>
2023-05-29 17:26:42 +0700

    Add checking steps

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,5 +9,27 @@ describe("basic form", () => {
     expect(screen.getByTestId("first")).toBeInTheDocument();
     expect(screen.getByTestId("last")).toBeInTheDocument();
     expect(screen.getByTestId("idno")).toBeInTheDocument();
+    expect(screen.getByTestId("submit-button")).toBeInTheDocument();
+    expect(screen.getByTestId("submit-button")).toBeDisabled();
   });
+
+  it("enabled the button upon filling the form", () => {
+    render(<Home />);
+    const fname = screen.getByLabelText('First name')
+    fireEvent.change(fname, { target: { value: 'Son' } })
+    const lname = screen.getByLabelText('Last name')
+    fireEvent.change(lname, { target: { value: 'Son' } })
+    const idno = screen.getByTestId("idno")
+    fireEvent.change(idno, { target: { value: '123456' } })
+    const submitButton = screen.getByTestId('submit-button')
+    expect(submitButton).toBeEnabled();
+    fireEvent.click(submitButton)
+    const email = screen.getByLabelText('Email')
+    fireEvent.change(email, { target: { value: 'abc@gmail.com' } })
+    const phoneNumber = screen.getByLabelText('Phone number')
+    fireEvent.change(phoneNumber, { target: { value: '123456' } })
+    const dateOfBirth = screen.getByLabelText('Date of birth')
+    fireEvent.change(dateOfBirth, { target: { value: '123456' } })
+    // expect(submitButton).toBeEnabled()
+  })
 });
